4894c72c4e80a0858d7696c1ed806e6aca328bf0
Prajapatipushkar <kppusakar2018@gmail.com>
2026-07-28 16:40:48 +0530

    Setup Tailwind CSS and React Router

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "../pages/Home";
+import Login from "../pages/Login";
+import Dashboard from "../pages/Dashboard";
+import NotFound from "../pages/NotFound";
+
+function AppRoutes() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default AppRoutes;
